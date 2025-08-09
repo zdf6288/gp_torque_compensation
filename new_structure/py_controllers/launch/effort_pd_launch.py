0c0a36a195fb765a4571d05b03da417d78429789
LearnerSebM <mengziyang168@gmail.com>
2025-08-09 17:24:14 +0200

    work at 2025-08-19, tempo ver of pd

--- new_structure/py_controllers/launch/effort_pd_launch.py
+++ new_structure/py_controllers/launch/effort_pd_launch.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
+from launch_ros.actions import Node, DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -18,3 +22,68 @@ def generate_launch_description():
             }]
         )
     ]) 
+
+def generate_launch_description():
+    robot_ip_parameter_name = 'robot_ip'
+    load_gripper_parameter_name = 'load_gripper'
+    use_fake_hardware_parameter_name = 'use_fake_hardware'
+    fake_sensor_commands_parameter_name = 'fake_sensor_commands'
+    use_rviz_parameter_name = 'use_rviz'
+
+    robot_ip = LaunchConfiguration(robot_ip_parameter_name)
+    load_gripper = LaunchConfiguration(load_gripper_parameter_name)
+    use_fake_hardware = LaunchConfiguration(use_fake_hardware_parameter_name)
+    fake_sensor_commands = LaunchConfiguration(fake_sensor_commands_parameter_name)
+    use_rviz = LaunchConfiguration(use_rviz_parameter_name)
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            robot_ip_parameter_name,
+            description='Hostname or IP address of the robot.'),
+        DeclareLaunchArgument(
+            use_rviz_parameter_name,
+            default_value='false',
+            description='Visualize the robot in Rviz'),
+        DeclareLaunchArgument(
+            use_fake_hardware_parameter_name,
+            default_value='false',
+            description='Use fake hardware'),
+        DeclareLaunchArgument(
+            fake_sensor_commands_parameter_name,
+            default_value='false',
+            description="Fake sensor commands. Only valid when '{}' is true".format(
+                use_fake_hardware_parameter_name)),
+        DeclareLaunchArgument(
+            load_gripper_parameter_name,
+            default_value='true',
+            description='Use Franka Gripper as an end-effector, otherwise, the robot is loaded '
+                        'without an end-effector.'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([PathJoinSubstitution(
+                [FindPackageShare('new_bringup'), 'launch', 'franka.launch.py'])]),
+            launch_arguments={robot_ip_parameter_name: robot_ip,
+                              load_gripper_parameter_name: load_gripper,
+                              use_fake_hardware_parameter_name: use_fake_hardware,
+                              fake_sensor_commands_parameter_name: fake_sensor_commands,
+                              use_rviz_parameter_name: use_rviz
+                              }.items(),
+        ),
+
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['cpp_relayer'],
+            output='screen',
+        ),
+        Node(
+            package='py_controllers',
+            executable='effort_pd',
+            name='effort_pd',
+            output='screen',
+            parameters=[{
+                'k_gains': [24.0, 24.0, 24.0, 24.0, 10.0, 6.0, 2.0],
+                'd_gains': [2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 0.5],
+            }]
+        )
+    ])
--- new_structure/py_controllers/py_controllers/effort_pd.py
+++ new_structure/py_controllers/py_controllers/effort_pd.py
@@ -19,12 +19,13 @@ class EffortPDController(Node):
         self.effort_publisher = self.create_publisher(
             EffortCommand, '/effort_command', 10)
         
-        # p_gain and d_gain, read from config, TO BE DONE
-        self.declare_parameter('k_gains', np.array([24.0, 24.0, 24.0, 24.0, 10.0, 6.0, 2.0]))
-        self.declare_parameter('d_gains', np.array([2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 0.5]))
+        self.declare_parameter('k_gains', [24.0, 24.0, 24.0, 24.0, 10.0, 6.0, 2.0])  # k_gains in PD control
+        self.declare_parameter('d_gains', [2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 0.5])       # d_gains in PD control
+        self.k_gains = np.array(self.get_parameter('k_gains').value, dtype=float)
+        self.d_gains = np.array(self.get_parameter('d_gains').value, dtype=float)
         
-        # 目标位置（可以根据需要修改）
-        # self.target_position = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.q_initial = None  # initial joint position q0
+        self.t_initial = None  # initial time
         
         self.effort_msg = EffortCommand()
         self.get_logger().info('Effort PD controller node started')
@@ -32,22 +33,40 @@ class EffortPDController(Node):
     def stateParameterCallback(self, msg):
         """callback function for /state_parameter subscriber"""
         try:
+            # initialize t_initial and obtain t_elapsed
+            t_now = self.get_clock().now()
+            if self.t_initial is None:
+                self.t_initial = t_now  
+                t_elapsed = 0.0
+            else:
+                t_elapsed = (t_now - self.t_initial).nanoseconds / 1e9
+            self.get_logger().debug(f"t_elapsed: {t_elapsed:.6f}s")
+
+            # initialize q_initial and obtain q and dq
             q = np.array(msg.position)
             dq = np.array(msg.velocity)
+            if self.q_initial is None:
+                self.q_initial = q.copy()
+                self.get_logger().info(f'Captured initial position q0: {self.q_initial.tolist()}')      
             
-            # # 计算位置误差
-            # position_error = self.target_position - current_position
+            # test trajectory, joint 4 and 5 move periodically
+            q_delta = np.pi / 8.0 * (1 - np.cos(np.pi / 2.5 * t_elapsed))
+            q_des = self.q_initial.copy()
+            q_des[3] += q_delta
+            q_des[4] += q_delta
+            dq_delta = np.pi / 2.5 * np.sin(np.pi / 2.5 * t_elapsed)
+            dq_des = np.zeros(7)
+            dq_des[3] = dq_delta
+            dq_des[4] = dq_delta
             
-            # # PD控制律
-            # effort = self.kp * position_error - self.kd * current_velocity
-            
-            # 限制力矩范围（可选）
-            effort = np.clip(effort, -100.0, 100.0)
+            # PD control 
+            tau = np.dot(self.k_gains, (q_des - q)) + np.dot(self.d_gains, (dq_des - dq))
+            tau = np.clip(tau, -100.0, 100.0)
             
             # Publish on topic /effort_command
-            self.effort_msg.efforts = effort.tolist()
+            self.effort_msg.efforts = tau.tolist()
             self.effort_publisher.publish(self.effort_msg)
-            self.get_logger().debug(f'published on topic /effort_command: {effort}')
+            self.get_logger().debug(f'published on topic /effort_command: {tau}')
             
         except Exception as e:
             self.get_logger().error(f'Parameter error: {str(e)}')
